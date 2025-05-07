aeb6a99a0d355df6f1bec52ce6a9828a2a2b5143
noidee <tim@noidee.de>
2025-05-07 11:31:19 +0200

    feat: improve scanner modal

--- resources/views/livewire/scanner.blade.php
+++ resources/views/livewire/scanner.blade.php
@@ -1,7 +1,7 @@
 <div>
     <x-filament::modal id="scanner-modal" width="3xl" close-event-name="scanner-modal-close">
         <x-slot name="trigger">
-            <x-filament::button color="success" id="scanner-start-button">
+            <x-filament::button color="success" id="scanner-start-button" >
                 Scanner benutzen
             </x-filament::button>
         </x-slot>
@@ -11,6 +11,20 @@
 
         <video id="scanner-area"></video>
 
+        <x-filament::input.wrapper :valid="!$errors->has('serialNumber')">
+            <x-filament::input class="border-gray-900" id="scanner-code" placeholder="Seriennummer eingeben" autofocus wire:model="serialNumber" wire:input.debounce.500ms="change" />
+        </x-filament::input.wrapper>
+    </x-filament::modal>
+
+    <x-filament::modal id="scanner-modal" width="3xl" close-event-name="scanner-modal-close">
+        <x-slot name="trigger">
+            <x-filament::button color="success" id="hand-scanner-start-button">
+                Hand Scanner benutzen
+            </x-filament::button>
+        </x-slot>
+        <x-slot name="heading">
+            Scan
+        </x-slot>
         <x-filament::input.wrapper :valid="!$errors->has('serialNumber')">
             <x-filament::input class="border-gray-900" id="scanner-code" placeholder="Seriennummer eingeben" autofocus wire:model="serialNumber" wire:input.debounce.500ms="change" />
         </x-filament::input.wrapper>
