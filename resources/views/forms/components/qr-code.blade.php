aecbd36528a774cebc508a017fe3348cdf62011b
Daniel Elskamp <ich@dginx.de>
2025-05-07 09:13:33 +0200

    fix: adjust padding and size of qr-code

--- resources/views/forms/components/qr-code.blade.php
+++ resources/views/forms/components/qr-code.blade.php
@@ -5,8 +5,8 @@
 @endphp
 
 @if ($id)
-    <div class="p-4 flex justify-end">
-        {!! QrCode::size(50)->generate($id) !!}
+    <div class="flex justify-end">
+        {!! QrCode::size(60)->generate($id) !!}
     </div>
 @else
     <p class="text-sm text-gray-500">QR-Code wird angezeigt, sobald das Modell gespeichert wurde.</p>
