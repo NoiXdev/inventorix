6d4e599b461313d958bf80a9647378c346f626b5
Daniel Elskamp <ich@dginx.de>
2026-05-10 20:46:58 +0200

    feat: install microsoft-azure socialite provider and add config

--- config/services.php
+++ config/services.php
@@ -35,4 +35,12 @@ return [
         ],
     ],
 
+    'microsoft-azure' => [
+        'enabled'       => filter_var(env('MS_LOGIN_ENABLED', false), FILTER_VALIDATE_BOOLEAN),
+        'client_id'     => env('MS_CLIENT_ID'),
+        'client_secret' => env('MS_CLIENT_SECRET'),
+        'redirect'      => env('MS_REDIRECT_URI'),
+        'tenant'        => env('MS_TENANT_ID'),
+    ],
+
 ];
