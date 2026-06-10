ba6adf3134938837694f0e9215e36e5408973075
Daniel Elskamp <ich@dginx.de>
2026-06-10 22:02:25 +0200

    feat(scanner): add inline error element and message data attributes

--- resources/views/livewire/scanner.blade.php
+++ resources/views/livewire/scanner.blade.php
@@ -9,7 +9,15 @@
             Scan
         </x-slot>
 
-        <video id="scanner-area"></video>
+        <div id="scanner-container"
+             data-error-permission="{{ trans('scanner.error.permission_denied') }}"
+             data-error-not-found="{{ trans('scanner.error.not_found') }}"
+             data-error-generic="{{ trans('scanner.error.generic') }}">
+            <video id="scanner-area"></video>
+            <p id="scanner-error"
+               role="alert"
+               class="hidden mt-2 text-sm text-danger-600 dark:text-danger-400"></p>
+        </div>
 
         <x-filament::input.wrapper :valid="!$errors->has('serialNumber')">
             <x-filament::input class="border-gray-900" id="scanner-code" placeholder="Seriennummer eingeben" autofocus wire:model="serialNumber" wire:input.debounce.500ms="change" />
